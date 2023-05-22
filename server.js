8d51b71b4b6f39026b7cf59442fa8d5592c1cc8f
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2023-05-21 23:27:17 -0500

    hello world application jamun tech

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const name = 'basanta';
-console.log(name);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+})
+
+app.listen(port,() => {
+    console.log(`Application running on port ${port}`);
+});
